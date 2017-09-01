5e12a3e6555afcb5514791735624167332f5a61a
KD <kamaldolikay@gmail.com>
2017-09-01 19:34:00 +0530

    generate commands for krypt only

--- bin/demo-global.js
+++ bin/demo-global.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+var fs          = require("fs");
 
 var myLibrary = require('../lib/index.js');
 var args = process.argv.splice(process.execArgv.length + 2);
@@ -12,13 +13,17 @@ if( args[0] == undefined){
 }
 
 if( args[0] == 'generate' ){
-    if( args[1] == 'model' || args[1] == 'controller' || args[1] == 'services' ){
-        if( args[2] !== undefined ){
-            myLibrary.say({data:args[0], g_type:args[2]});
+    if (fs.existsSync(process.cwd()+'/.kryptmaster')) {
+        if( args[1] == 'model' || args[1] == 'controller' || args[1] == 'service' ){
+            if( args[2] !== undefined ){
+                myLibrary.say({data:args[0], g_path:args[1]+'s', g_type:args[1], g_value:args[2]});
+            } else {
+                myLibrary.say({data:args[0],result:"seek help generate " + args[1]});
+            }
         } else {
-            myLibrary.say({data:args[0],result:"seek help generate " + args[1]});
+            myLibrary.say({data:args[0],result:"seek help generate"});
         }
     } else {
-        myLibrary.say({data:args[0],result:"seek help generate"});
+        console.log("You have to be inside an Krypt CLI project in order to use the generate command.");
     }
 }
